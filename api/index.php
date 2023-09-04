782eb57b9d41de1f8390ad2dbd20e62eab21557f
Guilherme Rocha <guilherme.rocha@linktou.com.br>
2023-09-04 19:11:23 -0300

    Fix css import path

--- api/index.php
+++ api/index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Calculador IMC</title>
-  <link href="./index.css" rel="stylesheet">
+  <link href="../index.css" rel="stylesheet">
   </link>
 </head>
 
